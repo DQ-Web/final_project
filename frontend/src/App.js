3be20e16b1179e7d2f60174bc77b035b48f6bb43
Quymbee Chen <quymbee@mattchen.com>
2021-07-08 00:03:15 -0400

    global navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,25 +8,27 @@ import {
   CampusContainer,
   StudentContainer,
   AllCampusesContainer,
-  AllStudentsContainer
-} from './components/containers';
+  AllStudentsContainer,
+} from "./components/containers";
+import Navbar from "./components/Nav";
 
-// if you create separate components for adding/editing 
+// if you create separate components for adding/editing
 // a student or campus, make sure you add routes to those
 // components here
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route exact path="/" component={HomePageContainer} />
         <Route exact path="/campuses" component={AllCampusesContainer} />
         <Route exact path="/campus/:id" component={CampusContainer} />
         <Route exact path="/students" component={AllStudentsContainer} />
         <Route exact path="/student/:id" component={StudentContainer} />
-      </Switch>        
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
