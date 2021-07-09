79684a179d6b5d5a022f1f87a6f02ac088b3eebc
Quymbee Chen <quymbee@mattchen.com>
2021-07-09 04:09:16 -0400

    working edit campus form

--- frontend/src/components/NewState/EditCampusContainer.js
+++ frontend/src/components/NewState/EditCampusContainer.js
@@ -2,13 +2,18 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import EditCampus from "./EditCampus";
-import { addCampusThunk } from "../../store/thunks";
+import {
+  addCampusThunk,
+  editCampusThunk,
+  fetchCampusThunk,
+} from "../../store/thunks";
 
 const defaultImg = "https://image.flaticon.com/icons/png/512/170/170427.png";
 
 class EditCampusContainer extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       name: "",
       address: "",
@@ -16,6 +21,7 @@ class EditCampusContainer extends Component {
       description: "",
       redirect: false,
       redirectId: null,
+      id: null,
     };
   }
 
@@ -27,15 +33,23 @@ class EditCampusContainer extends Component {
 
   handleSubmit = async (event) => {
     event.preventDefault();
+    const path = window.location.pathname;
+    const isEdit = path.includes("edit");
+    const id = path.slice(8, 9);
 
     let campus = {
       name: this.state.name,
       address: this.state.address,
       imageUrl: this.state.imageUrl,
       description: this.state.description,
+      id: isEdit ? id : null,
     };
 
-    let newCampus = await this.props.addCampus(campus);
+    console.log(campus);
+
+    let newCampus = (await isEdit)
+      ? this.props.editCampus(campus)
+      : this.props.addCampus(campus);
 
     this.setState({
       name: "",
@@ -43,7 +57,8 @@ class EditCampusContainer extends Component {
       imageUrl: defaultImg,
       description: "",
       redirect: true,
-      redirectId: newCampus.id,
+      redirectId: isEdit ? id : newCampus.id,
+      id: null,
     });
   };
 
@@ -52,12 +67,15 @@ class EditCampusContainer extends Component {
   }
 
   render() {
+    const isEdit = window.location.pathname.includes("edit");
+
     return this.state.redirect ? (
       <Redirect to={`/campus/${this.state.redirectId}`} />
     ) : (
       <EditCampus
         handleChange={this.handleChange}
         handleSubmit={this.handleSubmit}
+        isEdit={isEdit}
       />
     );
   }
@@ -66,6 +84,8 @@ class EditCampusContainer extends Component {
 const mapDispatch = (dispatch) => {
   return {
     addCampus: (campus) => dispatch(addCampusThunk(campus)),
+    editCampus: (campus) => dispatch(editCampusThunk(campus)),
+    fetchCampus: (campusId) => dispatch(fetchCampusThunk(campusId)),
   };
 };
 
